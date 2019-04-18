1591a866e2f990842e012ef6356b366c7be185bc
kimjuliana <juliana.kg@gmail.com>
2019-04-17 22:20:44 -0300

    aula3

--- 02/Ex01-news/js/script.js
+++ 02/Ex01-news/js/script.js
@@ -5,5 +5,18 @@ var botao = document.querySelector('.button');
 botao.addEventListener('click',function(evento){
     evento.preventDefault();
     var email = document.querySelector('#newsInputEmail')
-       mensagem.textContent = 'O email '+ email.value +' foi cadastrado com sucesso!'
+    if(email.value =="" || email.value.length == 0){
+        mensagem.textContent = 'Digite um email válido'
+        return
+    } else if
+        (email.value.indexOf('@')<=0 || email.value.indexOf('@')>=email.value.length-1){
+            mensagem.textContent = 'O email informado é inválido'
+            return
+    }
+
+        mensagem.textContent = 'O email '+ email.value +' foi cadastrado com sucesso!'
+  console.log(email.value.indexOf('@'))  
 })
+
+
+
